5a8715ea3aa76238f3ad6ac12629d4fbbfe1afc5
Igor Korabelnikov <i.korabel2001@gmail.com>
2021-10-10 23:08:36 +0200

    Initial Commit

---
+++ CleanWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanWeatherApp
+//
+//  Created by Игорь Корабельников on 10.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
