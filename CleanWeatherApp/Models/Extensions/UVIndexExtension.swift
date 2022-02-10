6b1b5f5b59aac4b78ee188988e55926cbe9d9214
Igor Korabelnikov <i.korabel2001@gmail.com>
2022-02-11 04:21:58 +0500

    some changes with files

--- CleanWeatherApp/Models/Extensions/UVIndexExtension.swift
+++ CleanWeatherApp/Models/Extensions/UVIndexExtension.swift
@@ -9,20 +9,15 @@ import Foundation
 
 extension UVindex {
     
+    /// Определить уровень опасности ультрафиолетового индекса
     func defineLevelOfUV() -> String {
         switch self {
-        case 0...2:
-            return "Low"
-        case 3...5:
-            return "Moderate"
-        case 6...7:
-            return "High"
-        case 8...10:
-            return "Very high"
-        case 11...100:
-            return "Extremely high"
-        default:
-            return ""
+        case 0...2: return "Low"
+        case 3...5: return "Moderate"
+        case 6...7: return "High"
+        case 8...10: return "Very high"
+        case 11...100: return "Extremely high"
+        default: return ""
         }
     }
     
